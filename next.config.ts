eac2d038a00ae26aeb3be0d01236ea45bb8923a1
Porramat Thaepngoen <porramat081@gmail.com>
2026-04-05 13:51:59 +0700

    Implement async rewrites for API routing
    Add async rewrites to configure API routing

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `{process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`, // The actual API
+      },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
